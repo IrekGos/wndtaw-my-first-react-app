380787f40e58072afb986a653f6d3237081767a1
IrekGos <ireneusz.goscinski@gmail.com>
2022-03-16 12:41:12 +0100

    poprawka formatowania

--- my-app/src/HelloWorld.tsx
+++ my-app/src/HelloWorld.tsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 export default class Hello extends Component {
     render() {
-        return (<div>
-            <p>Hello</p><p>World!</p></div>);
+        return (
+            <div>
+                <p>Hello</p>
+                <p>World!</p>
+            </div>
+        );
     }
 }
